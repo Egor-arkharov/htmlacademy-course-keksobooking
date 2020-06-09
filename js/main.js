3d8924ca96eb87b6c1cbcbf1e01cae59c57c825a
Egor-arkharov <egorarkharov@gmail.com>
2020-06-09 21:59:24 +0300

    Исправления 1

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 'use strict';
 
 var TITLES = ['2-комнтаные апартаменты в центре', 'Элитное жилье в районе Аракава', 'Новостройка в районе Кацусика (рядом с ж/д)', 'Двушка с видом на парк Инокасира', 'Квартира рядом с Императорским дворцом', 'Вторичное жилье рядом со станцией Тораномон', 'Квартира в Маруноути с паркингом', 'Двухэтажное жилье у реки Канда'];
-var PRICES = ['200', '500', '1000', '2500', '5200', '7550', '10900', '15000'];
-var ROOMS = ['1', '1', '2', '2', '2', '3', '4', '5'];
-var GUESTS = ['1', '1', '1', '2', '2', '3', '4', '6'];
 var TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var CHECKINS = ['12:00', '13:00', '14:00'];
 var CHECKOUTS = ['12:00', '13:00', '14:00'];
 var OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var OFFER_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var MIN_OFFER_VALUE = 1;
+var MAX_IMG_VALUE = 8;
+var MAX_PRICE_VALUE = 15000;
+var MAX_ROOMS_VALUE = 5;
+var MAX_GUESTS_VALUE = 6;
 
 var map = document.querySelector('.map');
 var pinTemplate = document.querySelector('#pin').content;
@@ -31,17 +33,16 @@ var getOfferValue = function (offerKey) {
 
 var getOfferArr = function (offerKey) {
   var offerArr = [];
-  var offerValue1 = getRandomNum(0, offerKey.length);
-  for (var i = 0; i <= offerValue1; i++) {
-    var offer1 = offerKey[i];
-    offerArr.push(offer1);
+  var randomArrLength = getRandomNum(0, offerKey.length);
+  for (var i = 0; i <= randomArrLength; i++) {
+    offerArr.push(offerKey[i]);
   }
 
   return offerArr;
 };
 
 var getApartment = function () {
-  var imgAddress = '0' + getRandomNum(1, 8);
+  var imgAddress = '0' + getRandomNum(MIN_OFFER_VALUE, MAX_IMG_VALUE);
   var locationX = getRandomNum(50, 500);
   var locationY = getRandomNum(130, 630);
 
@@ -50,14 +51,14 @@ var getApartment = function () {
       avatar: 'img/avatars/user' + imgAddress + '.png'
     },
     offer: {
-      title: String(getOfferValue(TITLES)),
-      address: String(locationX + ', ' + locationY),
-      price: getOfferValue(PRICES),
+      title: getOfferValue(TITLES),
+      address: locationX + ', ' + locationY,
+      price: getRandomNum(MIN_OFFER_VALUE, MAX_PRICE_VALUE),
       type: getOfferValue(TYPES),
-      rooms: getOfferValue(ROOMS),
-      guests: getOfferValue(GUESTS),
-      checkin: String(getOfferValue(CHECKINS)),
-      checkout: String(getOfferValue(CHECKOUTS)),
+      rooms: getRandomNum(MIN_OFFER_VALUE, MAX_ROOMS_VALUE),
+      guests: getRandomNum(MIN_OFFER_VALUE, MAX_GUESTS_VALUE),
+      checkin: getOfferValue(CHECKINS),
+      checkout: getOfferValue(CHECKOUTS),
       features: getOfferArr(OFFER_FEATURES),
       description: 'Сдается в аренду апартаменты с панорамными видами! Высокий этаж и потолки. Функциональная планировка: большая кухня-гостиная, спальня с гардеробной комнатой санузлом, прачечная, холл и гостевой санузел. Апартамент меблирован Итальянской мебелью и укомплектован необходимой техникой, готовы к проживанию.',
       photos: getOfferArr(OFFER_PHOTOS)
