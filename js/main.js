2185495cb96c0b563ac33e5e083b37f6536a17a0
Egor-arkharov <egorarkharov@gmail.com>
2020-06-23 18:13:05 +0300

    Попытка отрисовать разные карточки

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var TITLES = ['2-комнтаные апартаменты в центре', 'Элитное жилье в районе Аракава', 'Новостройка в районе Кацусика (рядом с ж/д)', 'Двушка с видом на парк Инокасира', 'Квартира рядом с Императорским дворцом', 'Вторичное жилье рядом со станцией Тораномон', 'Квартира в Маруноути с паркингом', 'Двухэтажное жилье у реки Канда'];
-var TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var CHECKINS = ['12:00', '13:00', '14:00'];
 var CHECKOUTS = ['12:00', '13:00', '14:00'];
 var OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
@@ -24,11 +23,6 @@ var mapPinMain = document.querySelector('.map__pin--main');
 var mapPins = document.querySelector('.map__pins');
 var filtersContainer = document.querySelector('.map__filters-container');
 
-var popupTemplate = document.querySelector('#card').content;
-var card = popupTemplate.querySelector('.popup');
-var apartmentCard = card.cloneNode(true);
-var buttonClose = apartmentCard.querySelector('.popup__close');
-
 var offsetX = (mapPin.getBoundingClientRect().width) / 2;
 var offsetY = mapPin.getBoundingClientRect().height;
 
@@ -44,18 +38,27 @@ var timeout = form.querySelector('#timeout');
 
 address.value = offsetX + ', ' + offsetY;
 
-var ApartmentTypesTranslate = {
-  BUNGALO: 'Бунгало',
-  FLAT: 'Квартира',
-  HOUSE: 'Дом',
-  PALACE: 'Дворец'
-};
-
-var apartmentPrices = {
-  bungalo: 0,
-  flat: 1000,
-  house: 5000,
-  palace: 10000
+var apartments = {
+  BUNGALO: {
+    type: 'BUNGALO',
+    name: 'Бунгало',
+    price: 0
+  },
+  FLAT: {
+    type: 'FLAT',
+    name: 'Квартира',
+    price: 1000
+  },
+  HOUSE: {
+    type: 'HOUSE',
+    name: 'Дом',
+    price: 5000
+  },
+  PALACE: {
+    type: 'PALACE',
+    name: 'Дворец',
+    price: 10000
+  }
 };
 
 var EvtKeys = {
@@ -89,6 +92,10 @@ var getApartment = function () {
   var locationX = getRandomNum(MIN_LOCATION_X, MAX_LOCATION_X);
   var locationY = getRandomNum(MIN_LOCATION_Y, MAX_LOCATION_Y);
 
+  var offerTypes = Object.values(apartments).map(function (value) {
+    return value.type;
+  });
+
   var apartmentOverview = {
     author: {
       avatar: 'img/avatars/user' + imgAddress + '.png'
@@ -97,7 +104,7 @@ var getApartment = function () {
       title: getOfferValue(TITLES),
       address: locationX + ', ' + locationY,
       price: getRandomNum(MIN_OFFER_VALUE, MAX_PRICE_VALUE),
-      type: getOfferValue(TYPES),
+      type: getOfferValue(offerTypes),
       rooms: getRandomNum(MIN_OFFER_VALUE, MAX_ROOMS_VALUE),
       guests: getRandomNum(MIN_OFFER_VALUE, MAX_GUESTS_VALUE),
       checkin: getOfferValue(CHECKINS),
@@ -143,10 +150,10 @@ var renderPin = function (newApartment) {
 
 var renderPins = function () {
   var fragment = document.createDocumentFragment();
-  var apartments = getApartments();
+  var apartments1 = getApartments();
 
   for (var i = 0; i < MAX_OFFER_QUANTITY; i++) {
-    var adPin = renderPin(apartments[i]);
+    var adPin = renderPin(apartments1[i]);
     fragment.appendChild(adPin);
   }
 
@@ -196,13 +203,11 @@ var validateCapacity = function () {
   }
 };
 
-var validatePrice = function () {
-  for (var i = 0; i < TYPES.length; i++) {
-    if (userType.value === TYPES[i]) {
-      userPrice.setAttribute('min', apartmentPrices[TYPES[i]]);
-      userPrice.setAttribute('placeholder', apartmentPrices[TYPES[i]]);
-    }
-  }
+var validatePrice = function (e) {
+  var value = e.target.value.toUpperCase();
+  userPrice.setAttribute('min', apartments[value].price);
+  userPrice.setAttribute('placeholder', apartments[value].price);
+
 };
 
 var validateTimeOut = function () {
@@ -221,56 +226,44 @@ userType.addEventListener('change', validatePrice);
 timein.addEventListener('change', validateTimeOut);
 timeout.addEventListener('change', validateTimeIn);
 
-var renderCardValue = function (popupValue, cardValue) {
-  apartmentCard.querySelector(popupValue).textContent = cardValue;
-};
+var popupTemplate = document.querySelector('#card').content;
+var card = popupTemplate.querySelector('.popup');
 
-var markCardFeatures = function () {
-  var offerFeatures = getApartment().offer.features;
-  var popupFeaturesList = popupTemplate.querySelector('.popup__features');
-  var popupFeaturesItems = popupFeaturesList.querySelectorAll('.popup__feature');
-  var popupDescription = apartmentCard.querySelector('.popup__description');
+var renderCard = function () {
+  var apartmentCard = card.cloneNode(true);
 
-  apartmentCard.querySelector('.popup__features').remove();
-
-  for (var i = popupFeaturesItems.length - 1; i >= offerFeatures.length; i--) {
-    popupFeaturesList.removeChild(popupFeaturesItems[i]);
-  }
-
-  apartmentCard.insertBefore(popupFeaturesList, popupDescription);
-};
-
-var markCardPhotos = function () {
+  var offerType = apartmentOffer.type.toUpperCase();
+  var offerFeatures = apartmentOffer.features;
+  var cardFeatures = apartmentCard.querySelectorAll('.popup__feature');
   var photosContainer = apartmentCard.querySelector('.popup__photos');
   var photos = photosContainer.querySelector('.popup__photo');
   var userPhotos = apartmentOffer.photos;
 
-  for (var i = 0; i < userPhotos.length; i++) {
-    photos.src = userPhotos[i];
+  apartmentCard.querySelector('.popup__title').textContent = apartmentOffer.title;
+  apartmentCard.querySelector('.popup__text--price').textContent = apartmentOffer.price + '₽/ночь';
+  apartmentCard.querySelector('.popup__type').textContent = apartments[offerType].name;
+  apartmentCard.querySelector('.popup__text--capacity').textContent = apartmentOffer.rooms + ' комнаты для ' + apartmentOffer.guests;
+  apartmentCard.querySelector('.popup__text--time').textContent = 'Заезд после ' + apartmentOffer.checkin + ', выезд до ' + apartmentOffer.checkout;
+  apartmentCard.querySelector('.popup__description').textContent = apartmentOffer.description;
+  apartmentCard.querySelector('.popup__avatar').src = apartment.author.avatar;
+
+  for (var i = 0; i < cardFeatures.length; i++) {
+    cardFeatures[i].classList.add('hidden');
+    for (var j = 0; j < offerFeatures.length; j++) {
+      cardFeatures[j].classList.remove('hidden');
+    }
+  }
+
+  for (var k = 0; k < userPhotos.length; k++) {
+    photos.src = userPhotos[k];
     var cloneImage = photos.cloneNode(true);
     photosContainer.appendChild(cloneImage);
   }
-};
-
-var markCardText = function () {
-  renderCardValue('.popup__title', apartmentOffer.title);
-  renderCardValue('.popup__text--price', apartmentOffer.price + '₽/ночь');
-  renderCardValue('.popup__type', ApartmentTypesTranslate[apartmentOffer.type.toUpperCase()]);
-  renderCardValue('.popup__text--capacity', apartmentOffer.rooms + ' комнаты для ' + apartmentOffer.guests);
-  renderCardValue('.popup__text--time', 'Заезд после ' + apartmentOffer.checkin + ', выезд до ' + apartmentOffer.checkout);
-  renderCardValue('.popup__description', apartmentOffer.description);
-  apartmentCard.querySelector('.popup__avatar').src = apartment.author.avatar;
-};
-
-var renderCard = function () {
-  markCardFeatures();
-  markCardPhotos();
-  markCardText();
 
   return apartmentCard;
 };
 
-var mapIcons = mapPins.querySelectorAll('.map__pin:not(.map__pin--main)');
+var newCard = renderCard();
 
 var onPopupEscPress = function (evt) {
   if (evt.key === EvtKeys.ESCAPE) {
@@ -279,19 +272,15 @@ var onPopupEscPress = function (evt) {
   }
 };
 
+var mapIcons = mapPins.querySelectorAll('.map__pin:not(.map__pin--main)');
+
 var openPopup = function () {
-  var newCard = renderCard();
   for (var i = 0; i < mapIcons.length; i++) {
     mapIcons[i].addEventListener('click', function () {
       map.insertBefore(newCard, filtersContainer);
       document.addEventListener('keydown', onPopupEscPress);
     });
   }
-};
-
-var closePopup = function () {
-  apartmentCard.remove();
-  mapPin.removeEventListener('click', openPopup);
 };
 
 mapPin.addEventListener('click', openPopup);
@@ -301,6 +290,13 @@ mapPin.addEventListener('keydown', function (evt) {
     openPopup();
   }
 });
+
+var closePopup = function () {
+  newCard.remove();
+  mapPin.removeEventListener('click', openPopup);
+};
+
+var buttonClose = newCard.querySelector('.popup__close');
 
 buttonClose.addEventListener('click', function () {
   closePopup();
